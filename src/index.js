45cc065e2afbd3f6a826ca473a6ef2dbd5dc90cf
gonzalote99 <gonzalodavila759@gmail.com>
2021-01-25 19:18:56 -0300

    3

--- src/index.js
+++ src/index.js
@@ -4,18 +4,15 @@ import ReactDom from 'react-dom'
 function Greeting() {
   return (
     <div>
-      <div>
-        <h4>gg</h4>
-        <ul>
-          <li>
-            <a href='#'>gol</a>
-          </li>
-          <input type='text' name='' id='' />
-        </ul>
-      </div>
-      <div></div>
+      <Person />
+      <Message />
     </div>
   )
+}
+
+const Person = () => <h2>ooo</h2>
+const Message = () => {
+  return <p>fdf</p>
 }
 
 ReactDom.render(<Greeting />, document.getElementById('root'))
