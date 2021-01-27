7ce26c6348221d54feedd3a89e9e2e02f8e6b172
gonzalote99 <gonzalodavila759@gmail.com>
2021-01-27 13:13:18 -0300

    6

--- src/index.js
+++ src/index.js
@@ -2,27 +2,30 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
 
+const author = 'fff'
+const title = 'op'
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/71L18q2vqCL._AC_UL160_SR111,160_.jpg'
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
+      <Book tr='car' />
+      <Book ot='sl' num={3} />
     </section>
   )
 }
 
-const author = 'fff'
-const Book = () => {
-  const title = 'op'
+const Book = (props) => {
   return (
     <article className='book'>
-      <img
-        src='https://images-na.ssl-images-amazon.com/images/I/71L18q2vqCL._AC_UL160_SR111,160_.jpg'
-        alt=''
-      />
+      <img src={img} alt='' />
 
       <h1>{title}</h1>
-      <h4>{author.toLocaleUpperCase()}</h4>
-      <p>{2 + 2}</p>
+      <h4>{author}</h4>
+      <p>{props.tr}</p>
+      <p>{props.ot}</p>
+      <p>{props.num}</p>
     </article>
   )
 }
