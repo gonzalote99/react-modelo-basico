d32ca2d86691e17c044f3907ddb858655d09fd84
gonzalote99 <gonzalodavila759@gmail.com>
2021-01-27 16:44:09 -0300

    9

--- src/index.js
+++ src/index.js
@@ -2,54 +2,51 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
 
-const firstBook = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/71L18q2vqCL._AC_UL160_SR111,160_.jpg',
-  title: 'op',
-  author: 'fff',
-}
-
-const secondBook = {
-  img:
-    'https://images-na.ssl-images-amazon.com/images/I/61OVIMnBy-L._AC_UL160_SR111,160_.jpg',
-  title: 'ophgg',
-  author: 'ffsff',
-}
+const books = [
+  {
+    id: 1,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71L18q2vqCL._AC_UL160_SR111,160_.jpg',
+    title: 'op',
+    author: 'fff',
+  },
+  {
+    id: 2,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/61OVIMnBy-L._AC_UL160_SR111,160_.jpg',
+    title: 'ophgg',
+    author: 'ffsff',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/7196WwHcD4L._AC_UL160_SR107,160_.jpg',
+    title: 'oawe',
+    author: 'fccff',
+  },
+]
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
-          repudiandae sapiente explicabo cum odio. Error alias cum porro quidem
-          iusto.
-        </p>
-      </Book>
-
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} book={book}></Book>
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, title, author, children } = props
   console.log(props)
+
+  const { img, title, author } = props.book
+
   return (
     <article className='book'>
       <img src={img} alt='' />
 
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
