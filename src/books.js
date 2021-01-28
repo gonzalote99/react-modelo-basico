7a69a4ff7594da08daa4e702b94aa47a6f86a40e
gonzalote99 <gonzalodavila759@gmail.com>
2021-01-28 17:58:08 -0300

    10

---
+++ src/books.js
@@ -0,0 +1,23 @@
+export const data = [
+  {
+    id: 1,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/71L18q2vqCL._AC_UL160_SR111,160_.jpg',
+    title: 'op',
+    author: 'fff',
+  },
+  {
+    id: 2,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/61OVIMnBy-L._AC_UL160_SR111,160_.jpg',
+    title: 'ophgg',
+    author: 'ffsff',
+  },
+  {
+    id: 3,
+    img:
+      'https://images-na.ssl-images-amazon.com/images/I/7196WwHcD4L._AC_UL160_SR107,160_.jpg',
+    title: 'oawe',
+    author: 'fccff',
+  },
+]
--- src/index.js
+++ src/index.js
@@ -2,52 +2,18 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
 
-const books = [
-  {
-    id: 1,
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/71L18q2vqCL._AC_UL160_SR111,160_.jpg',
-    title: 'op',
-    author: 'fff',
-  },
-  {
-    id: 2,
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/61OVIMnBy-L._AC_UL160_SR111,160_.jpg',
-    title: 'ophgg',
-    author: 'ffsff',
-  },
-  {
-    id: 3,
-    img:
-      'https://images-na.ssl-images-amazon.com/images/I/7196WwHcD4L._AC_UL160_SR107,160_.jpg',
-    title: 'oawe',
-    author: 'fccff',
-  },
-]
+import { data } from './books'
+import SpecificBook from './Book'
+import { greeting } from './testing/testing'
 
 function BookList() {
+  console.log(greeting)
   return (
     <section className='booklist'>
-      {books.map((book) => {
-        return <Book key={book.id} book={book}></Book>
+      {data.map((book, index) => {
+        return <SpecificBook key={book.id} {...book}></SpecificBook>
       })}
     </section>
-  )
-}
-
-const Book = (props) => {
-  console.log(props)
-
-  const { img, title, author } = props.book
-
-  return (
-    <article className='book'>
-      <img src={img} alt='' />
-
-      <h1>{title}</h1>
-      <h4>{author}</h4>
-    </article>
   )
 }
 
