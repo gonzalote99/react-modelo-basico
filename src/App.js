de9bffa0e93db1eec7337a1d93e564ac9aec42d5
gonzalote99 <gonzalodavila759@gmail.com>
2021-01-29 16:07:48 -0300

    12

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg'
 import './App.css'
 import Error from './Error.js'
+import UseStateArray from './UseStateArray'
+import UseStateObject from './UseStateObject'
 
 function App() {
   return (
     <div className='container'>
       <h2>alg</h2>
-      <Error />
+      <UseStateObject />
     </div>
   )
 }
