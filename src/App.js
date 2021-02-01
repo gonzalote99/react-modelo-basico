200d294abfd53e20b30023e362ae4557a06e9a5c
gonzalote99 <gonzalodavila759@gmail.com>
2021-02-01 15:03:40 -0300

    16

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Error from './Error.js'
 import UseStateArray from './UseStateArray'
 import UseStateObject from './UseStateObject'
 import UseStateCounter from './UseStateCounter'
+import UseEffectBasics1 from './UseEffectBasics1'
 
 function App() {
   return (
     <div className='container'>
       <h2>alg</h2>
-      <UseStateArray />
+      <UseEffectBasics1 />
     </div>
   )
 }
