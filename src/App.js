a4320c9962c3959ac8fa2eb498d9c1b7b1754a45
gonzalote99 <gonzalodavila759@gmail.com>
2021-02-01 18:51:02 -0300

    18

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import UseStateObject from './UseStateObject'
 import UseStateCounter from './UseStateCounter'
 import UseEffectBasics1 from './UseEffectBasics1'
 import UseEffectCleanup from './UseEffectCleanup'
+import UseEffectFetchData from './UseEffectFetchData'
 
 function App() {
   return (
     <div className='container'>
-      <UseEffectCleanup />
+      <UseEffectFetchData />
     </div>
   )
 }
