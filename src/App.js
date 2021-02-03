219b88d9c22f245453ac215da7b487fe4306fcd4
gonzalote99 <gonzalodavila759@gmail.com>
2021-02-03 18:16:28 -0300

    24

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import logo from './logo.svg'
 import './App.css'
 import UseStateCounter from './UseStateCounter'
 import UseEffectBasics1 from './UseEffectBasics1'
-import UseEffectCleanup from './UseEffectCleanup'
-import ShowHide from './ShowHide'
-import ControlledInputs1 from './ControlledInputs1'
+import UseRefBasics from './UseRefBasics'
 
 function App() {
   return (
     <div className='container'>
-      <ControlledInputs1 />
+      <UseRefBasics />
     </div>
   )
 }
