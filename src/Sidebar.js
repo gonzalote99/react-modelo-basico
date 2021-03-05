98135c80800b9c41454e0ac85d07ff8e26040afd
gonzalote99 <gonzalodavila759@gmail.com>
2021-03-05 15:45:20 -0300

    47

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,44 +1,42 @@
 import React from 'react'
-import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
-import { social, links } from './data'
 import { useGlobalContext } from './context'
-
+import sublinks from './data'
 const Sidebar = () => {
   const { isSidebarOpen, closeSidebar } = useGlobalContext()
-
   return (
-    <aside className={`${isSidebarOpen ? 'sidebar show-sidebar' : 'sidebar'} `}>
-      <div className='sidebar-header'>
-        <img src={logo} alt='logo' alt='coding addict' />
+    <div
+      className={`${
+        isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+      }`}
+    >
+      <aside className='sidebar'>
         <button className='close-btn' onClick={closeSidebar}>
           <FaTimes />
         </button>
-      </div>
-      <ul className='links'>
-        {links.map((link) => {
-          const { id, url, text, icon } = link
-          return (
-            <li key={id}>
-              <a href={url}>
-                {icon}
-                {text}
-              </a>
-            </li>
-          )
-        })}
-      </ul>
-      <ul className='social-icons'>
-        {social.map((link) => {
-          const { id, url, icon } = link
-          return (
-            <li key={id}>
-              <a href={url}>{icon}</a>
-            </li>
-          )
-        })}
-      </ul>
-    </aside>
+        <div className='sidebar-links'>
+          {sublinks.map((item, index) => {
+            const { links, page } = item
+            return (
+              <article key={index}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link, index) => {
+                    const { url, icon, label } = link
+                    return (
+                      <a key={index} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    )
+                  })}
+                </div>
+              </article>
+            )
+          })}
+        </div>
+      </aside>
+    </div>
   )
 }
 
--- src/context.js
+++ src/context.js
@@ -1,40 +1,48 @@
-import React, { useState, useContext } from 'react'
-
-const AppContext = React.createContext()
+import React, { useState, useContext } from 'react';
+import sublinks from './data';
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-  const [isModalOpen, setIsModalOpen] = useState(false)
-
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+  const [page, setPage] = useState({ page: '', links: [] });
+  const [location, setLocation] = useState({});
   const openSidebar = () => {
-    setIsSidebarOpen(true)
-  }
+    setIsSidebarOpen(true);
+  };
   const closeSidebar = () => {
-    setIsSidebarOpen(false)
-  }
-  const openModal = () => {
-    setIsModalOpen(true)
-  }
-  const closeModal = () => {
-    setIsModalOpen(false)
-  }
+    setIsSidebarOpen(false);
+  };
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page);
+    setLocation(coordinates);
+    setIsSubmenuOpen(true);
+  };
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
   return (
     <AppContext.Provider
       value={{
-        isModalOpen,
         isSidebarOpen,
-        openModal,
         openSidebar,
-        closeModal,
         closeSidebar,
+        isSubmenuOpen,
+        openSubmenu,
+        closeSubmenu,
+        page,
+        location,
       }}
     >
       {children}
     </AppContext.Provider>
-  )
-}
-
+  );
+};
+// make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
-export { AppContext, AppProvider }
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
